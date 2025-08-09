aa5b1824f77c2da5aa61fa995285cfb4f878ec5d
Zorropy <yeeemmmm@gmail.com>
2025-08-09 14:05:14 +0200

    Printed Menu, added User Input and Exit Function

--- movies.py
+++ movies.py
@@ -1,5 +1,10 @@
+def exit_program(movies):
+    print("Bye!!!")
+    exit()
+
 
 def main():
+    func_dict = {0: exit_program}
     # Dictionary to store the movies and the rating
     movies = {
         "The Shawshank Redemption": 9.5,
@@ -13,11 +18,31 @@ def main():
         "Forrest Gump": 8.8,
         "Star Wars: Episode V": 8.7
     }
+    while True:
+        print("""********** My Movies Database **********
 
-    # Your code here
+Menu:
+1. List movies
+2. Add movie
+3. Delete movie
+4. Update movie
+5. Stats
+6. Random movie
+7. Search movie
+8. Movies sorted by rating
+0. Exit
 
+Enter choice (0-8): """, end="")
+        try:
+            user_input = int(input())
+            if not 0 <= user_input <= 8:
+                raise ValueError
+            func_dict[user_input](movies)
+        except ValueError:
+            print("Error. Please choose a number between 0-8.")
+
+        input("Press enter to continue...")
 
 
 if __name__ == "__main__":
     main()
-
